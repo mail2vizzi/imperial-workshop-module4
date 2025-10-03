0cf4071914653ef8ba7b48677eb4643a96d1455f
Vijay <mail2vizzi@gmail.com>
2025-10-02 23:57:27 -0400

    @Vijay: unit test switchingTheCameraOnPowersUpTheSensor implemented.

--- src/main/test/com/vijay/module3/mock/CameraTest.java
+++ src/main/test/com/vijay/module3/mock/CameraTest.java
@@ -1,6 +1,9 @@
 package com.vijay.module3.mock;
 
+import org.jmock.Expectations;
+import org.jmock.Mockery;
 import org.jmock.integration.junit4.JUnitRuleMockery;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
@@ -9,9 +12,25 @@ public class CameraTest {
     @Rule
     public JUnitRuleMockery context = new JUnitRuleMockery();
 
+    private Camera camera;
+
+    private Sensor sensor;
+    private MemoryCard memoryCard;
+
+    @Before
+    public void setup(){
+        sensor = context.mock(Sensor.class);
+        camera = new Camera();
+    }
+
     @Test
     public void switchingTheCameraOnPowersUpTheSensor() {
-         // Write your test here
+
+        context.checking(new Expectations(){{
+            exactly(1).of(sensor).powerUp();
+        }});
+        camera.powerOn();
+        context.assertIsSatisfied();
     }
 
 }
