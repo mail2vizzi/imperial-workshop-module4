c24879e9d17b4f604f190651619c0a4a087010f7
Vijay <mail2vizzi@gmail.com>
2025-10-03 11:22:26 -0400

    @Vijay: Added comments to the Camera class.

--- src/main/java/com/vijay/module3/mock/Camera.java
+++ src/main/java/com/vijay/module3/mock/Camera.java
@@ -2,6 +2,13 @@ package com.vijay.module3.mock;
 
 import java.util.concurrent.atomic.AtomicBoolean;
 
+
+/**
+ * The Camera class coordinates interactions between a Sensor and a MemoryCard.
+ * It powers the sensor on/off and manages the process of capturing data
+ * and writing it to the memory card.
+ * Implements WriteListener so it can be notified when data writing is complete.
+ */
 public class Camera implements WriteListener {
 
     private final Sensor sensor;
@@ -14,6 +21,12 @@ public class Camera implements WriteListener {
         this.memoryCard = memoryCard;
     }
 
+    /**
+     * Pressing the shutter will capture data from the sensor and
+     * initiate writing it to the memory card, but only if:
+     * - The camera is powered on
+     * - The camera is not already writing data
+     */
     public void pressShutter() {
         if (isPowerOn.get() && !isWritingOn.get()) {
             isWritingOn.set(true);
@@ -21,6 +34,11 @@ public class Camera implements WriteListener {
         }
     }
 
+    /**
+     * Powering on the camera:
+     * - Sets the power flag to true
+     * - Powers up the sensor (if not already on)
+     */
     public void powerOn() {
         if (!isPowerOn.get()) {
             isPowerOn.set(true);
@@ -28,6 +46,13 @@ public class Camera implements WriteListener {
         }
     }
 
+    /**
+     * Powering off the camera:
+     * - Sets the power flag to false
+     * - If no write is in progress, powers down the sensor immediately
+     * - If writing is in progress, the sensor will stay powered on
+     *   until the write completes (handled in writeComplete)
+     */
     public void powerOff() {
         if (isPowerOn.get()) {
             isPowerOn.set(false);
@@ -37,6 +62,11 @@ public class Camera implements WriteListener {
         }
     }
 
+    /**
+     * Callback from the MemoryCard when writing is complete.
+     * - Clears the writing flag
+     * - If the camera was powered off while writing, powers down the sensor now
+     */
     @Override
     public void writeComplete() {
         if(isWritingOn.get()){
